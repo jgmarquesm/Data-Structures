e3adf73fb3ef572cfb77bfd7556925b5dd4aad9e
jgmarquesm <joao.mendes@zgsolucoes.com.br>
2023-09-14 01:15:24 -0300

    feat: Adiciona flag suppress ao helper ExceptionHandler para suprimir a impressão da error_message. Para manter compatibilidade e deixar o usuário escolher se quer ou não suprimir a mensagem de error, foi implementado um mecanismo de valor default para argumentos das funções desse helper.

--- resources/helpers/ExceptionHandler/main/src/exception_handler.c
+++ resources/helpers/ExceptionHandler/main/src/exception_handler.c
@@ -25,76 +25,130 @@ char *_build_error_message(const char *function_name, const char* field_name, co
     return error_message;
 }
 
-ExceptionResponse *_er_create(bool throws, char *error_message) {
+ExceptionResponse *_er_create(bool throws, char *error_message, bool suppress) {
     ExceptionResponse *exceptionResponse = (ExceptionResponse *) calloc(1, sizeof(ExceptionResponse));
     exceptionResponse->throws = throws;
     exceptionResponse->error_message = error_message;
+    exceptionResponse->suppress = suppress;
     return exceptionResponse;
 }
 
-ExceptionResponse *ExceptionHandler_is_non_positive(char *function, char *field, long value, bool includesZero) {
+ExceptionResponse *f1(char *function, char *field, long value, bool includesZero, bool suppress) {
     bool throws = false;
     char *error_message = "";
     if ((value == 0 && !includesZero) || value < 0) {
         throws = true;
         error_message = _build_error_message(function, field, " must be positive.\n");
     }
-    return _er_create(throws, error_message);
+    return _er_create(throws, error_message, suppress);
 }
 
-ExceptionResponse *ExceptionHandler_is_null(char *function, char *field, void *value) {
+ExceptionResponse *f2(char *function, char *field, void *value, bool suppress) {
     bool throws = false;
     char *error_message = "";
     if (value == NULL) {
         throws = true;
         error_message = _build_error_message(function, field, " is NULL.\n");
     }
-    return _er_create(throws, error_message);
+    return _er_create(throws, error_message, suppress);
 }
 
-ExceptionResponse *ExceptionHandler_is_empty(char *function, char *field, void *value, bool (*is_empty_function)(void *data)) {
+ExceptionResponse *f3(char *function, char *field, void *value, bool (*is_empty_function)(void *data), bool suppress) {
     bool throws = false;
     char *error_message = "";
     if (is_empty_function(value))  {
         throws = true;
         error_message = _build_error_message(function, field, " is empty.\n");
     }
-    return _er_create(throws, error_message);
+    return _er_create(throws, error_message, suppress);
 }
 
-ExceptionResponse *ExceptionHandler_is_full(char *function, char *field, void *value, bool (*is_full_function)(void *data)) {
+ExceptionResponse *f4(char *function, char *field, void *value, bool (*is_full_function)(void *data), bool suppress) {
     bool throws = false;
     char *error_message = "";
     if (is_full_function(value)) {
         throws = true;
         error_message = _build_error_message(function, field, " is full.\n");
     }
-    return _er_create(throws, error_message);
+    return _er_create(throws, error_message, suppress);
 }
 
-ExceptionResponse *ExceptionHandler_is_not_sorted(char *function, char *field, void *value, bool (*is_sorted_function)(void *data)) {
+ExceptionResponse *f5(char *function, char *field, void *value, bool (*is_sorted_function)(void *data), bool suppress) {
     bool throws = false;
     char *error_message = "";
     if (!is_sorted_function(value)) {
         throws = true;
         error_message = _build_error_message(function, field, " is not sorted.\n");
     }
-    return _er_create(throws, error_message);
+    return _er_create(throws, error_message, suppress);
 }
 
-ExceptionResponse *ExceptionHandler_is_out_of_bounds(char *function, char *field, long wanted_index, long max_index) {
+ExceptionResponse *f6(char *function, char *field, long value, long max_index, bool suppress) {
     bool throws = false;
     char *error_message = "";
-    if (wanted_index < 0 || wanted_index > max_index) {
+    if (value < 0 || value > max_index) {
         throws = true;
         error_message = _build_error_message(function, field, " is Out Of Bounds.\n");
     }
-    return _er_create(throws, error_message);
+    return _er_create(throws, error_message, suppress);
+}
+
+function_declaration(ExceptionResponse *, ExceptionHandler_is_non_positive) {
+    char *function = args.function;
+    char *field = args.field;
+    long value = args.value;
+    bool set_var(includesZero, false)
+    bool set_var(suppress, false)
+    return f1(function, field, value, includesZero, suppress);
+}
+
+function_declaration(ExceptionResponse *, ExceptionHandler_is_null) {
+    char *function = args.function;
+    char *field = args.field;
+    void *value = args.value;
+    bool set_var(suppress, false)
+    return f2(function, field, value, suppress);
+}
+
+function_declaration(ExceptionResponse *, ExceptionHandler_is_empty) {
+    char *function = args.function;
+    char *field = args.field;
+    void *value = args.value;
+    bool (* function_to_apply)(void *) = args.function_to_apply;
+    bool set_var(suppress, false)
+    return f3(function, field, value, function_to_apply ,suppress);
+}
+
+function_declaration(ExceptionResponse *, ExceptionHandler_is_full) {
+    char *function = args.function;
+    char *field = args.field;
+    void *value = args.value;
+    bool (* function_to_apply)(void *) = args.function_to_apply;
+    bool set_var(suppress, false)
+    return f4(function, field, value, function_to_apply ,suppress);
+}
+
+function_declaration(ExceptionResponse *, ExceptionHandler_is_not_sorted) {
+    char *function = args.function;
+    char *field = args.field;
+    void *value = args.value;
+    bool (* function_to_apply)(void *) = args.function_to_apply;
+    bool set_var(suppress, false)
+    return f5(function, field, value, function_to_apply ,suppress);
+}
+
+function_declaration(ExceptionResponse *, ExceptionHandler_is_out_of_bounds) {
+    char *function = args.function;
+    char *field = args.field;
+    long value = args.value;
+    long max_index = args.max_index;
+    bool set_var(suppress, false)
+    return f6(function, field, value, max_index, suppress);
 }
 
 bool anyThrows(int argc, ExceptionResponse *er, ...) {
     if (er->throws) {
-        fprintf(stderr, "%s", er->error_message);
+        if (!er->suppress) fprintf(stderr, "%s", er->error_message);
         return true;
     } else {
         if (argc == 1) return false;
@@ -104,7 +158,7 @@ bool anyThrows(int argc, ExceptionResponse *er, ...) {
         for (int i = 1; i < argc; i++) {
             e = va_arg(args, ExceptionResponse *);
             if (e->throws) {
-                fprintf(stderr, "%s", e->error_message);
+                if (!e->suppress) fprintf(stderr, "%s", e->error_message);
                 return true;
             }
         }
