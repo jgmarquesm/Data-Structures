fa399ed10c6ccbcefc35539ffda8384cbbab3440
jgmarquesm <joao.mendes@zgsolucoes.com.br>
2023-10-02 18:12:17 -0300

    feat: issue#48 aumenta cobertura de testes da Static Stack

--- main/StaticStack/main/src/static_stack.c
+++ main/StaticStack/main/src/static_stack.c
@@ -16,105 +16,109 @@ StaticStack *StaticStack_create(const long capacity, unsigned int size_of_type) 
             ExceptionHandler_is_non_positive("StaticStack_create", "Capacity", capacity, true, SUPPRESS_PRINT_ERROR)
         )
     ) return NULL;
-    StaticStack *staticStack = (StaticStack *) calloc(1, sizeof(StaticStack));
-    staticStack->data = Array_create(capacity, size_of_type);
-    return staticStack;
+    StaticStack *stack = (StaticStack *) calloc(1, sizeof(StaticStack));
+    stack->data = Array_create(capacity, size_of_type);
+    return stack;
 }
 
-void StaticStack_clean(StaticStack *staticStack) {
+bool StaticStack_clean(StaticStack *stack) {
     if (anyThrows(
             1,
-            ExceptionHandler_is_null("StaticStack_clean", "Static Stack", (void *) staticStack, SUPPRESS_PRINT_ERROR)
-        )
-    ) return;
-    Array_clean(staticStack->data);
-}
-
-void StaticStack_destroy(StaticStack **staticStack_ref) {
-    StaticStack *staticStack = *staticStack_ref;
-    if (anyThrows(
-            1,
-            ExceptionHandler_is_null("StaticStack_destroy", "Static Stack", (void *) staticStack, SUPPRESS_PRINT_ERROR)
-        )
-    ) return;
-    Array_delete(&(staticStack->data));
-    free(staticStack);
-    *staticStack_ref = NULL;
-}
-
-bool StaticStack_is_empty(void *staticStack) {
-    if (anyThrows(
-            1,
-            ExceptionHandler_is_null("StaticStack_is_empty", "Static Stack", (void *) staticStack, SUPPRESS_PRINT_ERROR)
-        )
-    ) return true;
-    return Array_is_empty(((StaticStack *) staticStack)->data);
-}
-
-bool StaticStack_is_full(void *staticStack) {
-    if (anyThrows(
-            1,
-            ExceptionHandler_is_null("StaticStack_is_full", "Static Stack", (void *) staticStack, SUPPRESS_PRINT_ERROR)
+            ExceptionHandler_is_null("StaticStack_clean", "Static Stack", (void *) stack, SUPPRESS_PRINT_ERROR)
         )
     ) return false;
-    return Array_is_full(((StaticStack *) staticStack)->data);
+    Array_clean(stack->data);
+    return true;
 }
 
-void StaticStack_push(StaticStack *staticStack, void *data) {
+bool StaticStack_destroy(StaticStack **stack_ref) {
+    StaticStack *stack = *stack_ref;
+    if (anyThrows(
+            1,
+            ExceptionHandler_is_null("StaticStack_destroy", "Static Stack", (void *) stack, SUPPRESS_PRINT_ERROR)
+        )
+    ) return false;
+    Array_delete(&(stack->data));
+    free(stack);
+    *stack_ref = NULL;
+    return true;
+}
+
+bool StaticStack_is_empty(void *stack) {
+    if (anyThrows(
+            1,
+            ExceptionHandler_is_null("StaticStack_is_empty", "Static Stack", (void *) stack, SUPPRESS_PRINT_ERROR)
+        )
+    ) return true;
+    return Array_is_empty(((StaticStack *) stack)->data);
+}
+
+bool StaticStack_is_full(void *stack) {
+    if (anyThrows(
+            1,
+            ExceptionHandler_is_null("StaticStack_is_full", "Static Stack", (void *) stack, SUPPRESS_PRINT_ERROR)
+        )
+    ) return false;
+    return Array_is_full(((StaticStack *) stack)->data);
+}
+
+bool StaticStack_push(StaticStack *stack, void *data) {
     if (anyThrows(
             2,
-            ExceptionHandler_is_null("StaticStack_push", "Static Stack", (void *) staticStack, SUPPRESS_PRINT_ERROR),
+            ExceptionHandler_is_null("StaticStack_push", "Static Stack", (void *) stack, SUPPRESS_PRINT_ERROR),
             ExceptionHandler_is_null("StaticStack_push", "Data", data, SUPPRESS_PRINT_ERROR)
         )
-    ) return;
-    Array_add_last(staticStack->data, data);
+    ) return false;
+    Array_add_last(stack->data, data);
+    return true;
 }
 
-void *StaticStack_peek(const StaticStack *staticStack) {
+void *StaticStack_peek(const StaticStack *stack) {
     if (anyThrows(
             2,
-            ExceptionHandler_is_null("StaticStack_peek", "Static Stack", (void *) staticStack, SUPPRESS_PRINT_ERROR),
-            ExceptionHandler_is_empty("StaticStack_peek", "Data", (void *) staticStack, StaticStack_is_empty, SUPPRESS_PRINT_ERROR)
+            ExceptionHandler_is_null("StaticStack_peek", "Static Stack", (void *) stack, SUPPRESS_PRINT_ERROR),
+            ExceptionHandler_is_empty("StaticStack_peek", "Data", (void *) stack, StaticStack_is_empty, SUPPRESS_PRINT_ERROR)
         )
     ) return NULL;
-    long top = Array_size(staticStack->data) - 1;
-    return Array_get_at(staticStack->data, top);
+    return Array_last_element(stack->data);
 }
 
-void StaticStack_pop(StaticStack *staticStack) {
+void *StaticStack_pop(StaticStack *stack) {
     if (anyThrows(
             2,
-            ExceptionHandler_is_null("StaticStack_pop", "Static Stack", (void *) staticStack, SUPPRESS_PRINT_ERROR),
-            ExceptionHandler_is_empty("StaticStack_pop", "Data", (void *) staticStack, StaticStack_is_empty, SUPPRESS_PRINT_ERROR)
+            ExceptionHandler_is_null("StaticStack_pop", "Static Stack", (void *) stack, SUPPRESS_PRINT_ERROR),
+            ExceptionHandler_is_empty("StaticStack_pop", "Data", (void *) stack, StaticStack_is_empty, SUPPRESS_PRINT_ERROR)
         )
-    ) return;
-    Array_remove_last(staticStack->data);
+    ) return NULL;
+    void *popped = Array_last_element(stack->data);
+    Array_remove_last(stack->data);
+    return popped;
 }
 
-void StaticStack_print(const StaticStack *staticStack, void (*type_print_function)(void * data)) {
+void StaticStack_print(const StaticStack *stack, void (*type_print_function)(void * data)) {
     if (anyThrows(
             2,
-            ExceptionHandler_is_null("StaticStack_print", "Static Stack", (void *) staticStack, SUPPRESS_PRINT_ERROR),
-            ExceptionHandler_is_empty("StaticStack_print", "Data", (void *) staticStack, StaticStack_is_empty, SUPPRESS_PRINT_ERROR)
+            ExceptionHandler_is_null("StaticStack_print", "Static Stack", (void *) stack, SUPPRESS_PRINT_ERROR),
+            ExceptionHandler_is_empty("StaticStack_print", "Data", (void *) stack, StaticStack_is_empty, SUPPRESS_PRINT_ERROR)
         )
     ) return;
-    printf("\nCapacity: %ld. Top: %ld.\n", Array_capacity(staticStack->data), Array_size(staticStack->data) - 1);
+    printf("\nCapacity: %ld. Top: %ld.\n", Array_capacity(stack->data), Array_size(stack->data) - 1);
     printf("-:[");
-    long size = Array_size(staticStack->data);
+    long size = Array_size(stack->data);
     for(long i = 0; i < size - 1; i++) {
-        type_print_function(Array_get_at(staticStack->data, i));
+        type_print_function(Array_get_at(stack->data, i));
         printf(", ");
     }
-    type_print_function(Array_get_at(staticStack->data, Array_size(staticStack->data) - 1));
+    type_print_function(Array_get_at(stack->data, Array_size(stack->data) - 1));
     puts("]:-");
 }
 
-long StaticStack_size(const StaticStack *staticStack) {
+long StaticStack_size(const StaticStack *stack) {
     if (anyThrows(
             2,
-            ExceptionHandler_is_null("StaticStack_size", "Static Stack", (void *) staticStack, SUPPRESS_PRINT_ERROR),
-            ExceptionHandler_is_empty("StaticStack_size", "Data", (void *) staticStack, StaticStack_is_empty, SUPPRESS_PRINT_ERROR)
+            ExceptionHandler_is_null("StaticStack_size", "Static Stack", (void *) stack, SUPPRESS_PRINT_ERROR),
+            ExceptionHandler_is_empty("StaticStack_size", "Data", (void *) stack, StaticStack_is_empty, SUPPRESS_PRINT_ERROR)
         )
     ) return 0;
-    return Array_size(staticStack->data);
+    return Array_size(stack->data);
 }
