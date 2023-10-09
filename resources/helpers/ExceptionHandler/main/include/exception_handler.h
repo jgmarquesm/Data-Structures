62149dad2c0faa9871c69f5af6f68cedefd53dd9
jgmarquesm <joao.mendes@zgsolucoes.com.br>
2023-10-09 18:12:36 -0300

    refactor: issue#58 Code Review ExceptionHandler

--- resources/helpers/ExceptionHandler/main/include/exception_handler.h
+++ resources/helpers/ExceptionHandler/main/include/exception_handler.h
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-#define SUPPRESS_PRINT_ERROR false
+#define __SUPPRESS_PRINT_ERROR__ false
 
 // See: https://modelingwithdata.org/arch/00000022.htm
 #define function_declaration(type, name) \
@@ -17,7 +17,7 @@
                 } variadic_type_##name;     \
     function_declaration(type, name);
 #define set_var(arg, value) arg = args.arg ? args.arg : (value);
-#define function_link(name,...) \
+#define function_link(name, ...) \
         variadic_##name((variadic_type_##name) {__VA_ARGS__})
 
 typedef struct _exception_response {
@@ -28,27 +28,27 @@ typedef struct _exception_response {
 
 ExceptionResponse *ExceptionHandler_is_non_positive(char *function, char *field, long value, bool includesZero, bool suppress);
 variadic_declare(ExceptionResponse *, ExceptionHandler_is_non_positive, char *function; char *field; long value; bool includesZero; bool suppress;)
-#define ExceptionHandler_is_non_positive(...) function_link(ExceptionHandler_is_non_positive,__VA_ARGS__)
+#define ExceptionHandler_is_non_positive(...) function_link(ExceptionHandler_is_non_positive, __VA_ARGS__)
 
 ExceptionResponse *ExceptionHandler_is_null(char *function, char *field, void *value, bool suppress);
 variadic_declare(ExceptionResponse *, ExceptionHandler_is_null, char *function; char *field; void *value; bool suppress;)
-#define ExceptionHandler_is_null(...) function_link(ExceptionHandler_is_null,__VA_ARGS__)
+#define ExceptionHandler_is_null(...) function_link(ExceptionHandler_is_null, __VA_ARGS__)
 
 ExceptionResponse *ExceptionHandler_is_empty(char *function, char *field, void *value, bool (*is_empty_function)(void *data), bool suppress);
 variadic_declare(ExceptionResponse *, ExceptionHandler_is_empty, char *function; char *field; void *value; bool (*function_to_apply)(void *data); bool suppress;)
-#define ExceptionHandler_is_empty(...) function_link(ExceptionHandler_is_empty,__VA_ARGS__)
+#define ExceptionHandler_is_empty(...) function_link(ExceptionHandler_is_empty, __VA_ARGS__)
 
 ExceptionResponse *ExceptionHandler_is_full(char *function, char *field, void *value, bool (*is_full_function)(void *data), bool suppress);
 variadic_declare(ExceptionResponse *, ExceptionHandler_is_full, char *function; char *field; void *value; bool (*function_to_apply)(void *data); bool suppress;)
-#define ExceptionHandler_is_full(...) function_link(ExceptionHandler_is_full,__VA_ARGS__)
+#define ExceptionHandler_is_full(...) function_link(ExceptionHandler_is_full, __VA_ARGS__)
 
 ExceptionResponse *ExceptionHandler_is_not_sorted(char *function, char *field, void *value, bool (*is_sorted_function)(void *data), bool suppress);
 variadic_declare(ExceptionResponse *, ExceptionHandler_is_not_sorted, char *function; char *field; void *value; bool (*function_to_apply)(void *data); bool suppress;)
-#define ExceptionHandler_is_not_sorted(...) function_link(ExceptionHandler_is_not_sorted,__VA_ARGS__)
+#define ExceptionHandler_is_not_sorted(...) function_link(ExceptionHandler_is_not_sorted, __VA_ARGS__)
 
 ExceptionResponse *ExceptionHandler_is_out_of_bounds(char *function, char *field, long value, long max_index, bool suppress);
 variadic_declare(ExceptionResponse *, ExceptionHandler_is_out_of_bounds, char *function; char *field; long value; long max_index; bool suppress;)
-#define ExceptionHandler_is_out_of_bounds(...) function_link(ExceptionHandler_is_out_of_bounds,__VA_ARGS__)
+#define ExceptionHandler_is_out_of_bounds(...) function_link(ExceptionHandler_is_out_of_bounds, __VA_ARGS__)
 
 bool anyThrows(int argc, ExceptionResponse *er, ...);
 
--- resources/helpers/ExceptionHandler/test/tests/exception_handler.test.c
+++ resources/helpers/ExceptionHandler/test/tests/exception_handler.test.c
@@ -7,8 +7,8 @@
 #define FUNCTION "std_function"
 #define FIELD "std_field"
 #define NOT_NULL ((void *) 1)
-#define THROWS 1
-#define NOT_THROWS 0
+#define THROWS true
+#define NOT_THROWS false
 #define NOT_THROWS_MSG ""
 #define NEGATIVE_THROWS_MSG "\nERROR: on function 'std_function'.\nERROR MESSAGE: std_field must be positive.\n"
 #define NULL_THROWS_MSG "\nERROR: on function 'std_function'.\nERROR MESSAGE: std_field is NULL.\n"
@@ -23,7 +23,6 @@
 #define INDEX_FIVE 5
 #define INCLUDES_ZERO true
 #define NOT_INCLUDES_ZERO false
-#define SUPPRESS true
 
 bool MOCK_is_empty_or_is_full_or_is_sorted(void *data) {
     return data == NULL;
@@ -35,7 +34,7 @@ void tearDown(){}
 
 void test_ExceptionHandler_is_non_positive_1() {
     TEST_MESSAGE("Case 1 --> value = -1, includesZero = false:");
-    ExceptionResponse *er = ExceptionHandler_is_non_positive(FUNCTION, FIELD, VALUE_NEGATIVE_1, NOT_INCLUDES_ZERO, SUPPRESS);
+    ExceptionResponse *er = ExceptionHandler_is_non_positive(FUNCTION, FIELD, VALUE_NEGATIVE_1, NOT_INCLUDES_ZERO, __SUPPRESS_PRINT_ERROR__);
     int message_comparison = strcmp(NEGATIVE_THROWS_MSG, er->error_message);
     TEST_ASSERT_EQUAL(THROWS, er->throws);
     TEST_ASSERT_EQUAL(0, message_comparison);
@@ -43,7 +42,7 @@ void test_ExceptionHandler_is_non_positive_1() {
 
 void test_ExceptionHandler_is_non_positive_2() {
     TEST_MESSAGE("Case 2 --> value = -1, includesZero = true:");
-    ExceptionResponse *er = ExceptionHandler_is_non_positive(FUNCTION, FIELD, VALUE_NEGATIVE_1, INCLUDES_ZERO, SUPPRESS);
+    ExceptionResponse *er = ExceptionHandler_is_non_positive(FUNCTION, FIELD, VALUE_NEGATIVE_1, INCLUDES_ZERO, __SUPPRESS_PRINT_ERROR__);
     int message_comparison = strcmp(NEGATIVE_THROWS_MSG, er->error_message);
     TEST_ASSERT_EQUAL(THROWS, er->throws);
     TEST_ASSERT_EQUAL(0, message_comparison);
@@ -51,7 +50,7 @@ void test_ExceptionHandler_is_non_positive_2() {
 
 void test_ExceptionHandler_is_non_positive_3() {
     TEST_MESSAGE("Case 3 --> value =  0, includesZero = false:");
-    ExceptionResponse *er = ExceptionHandler_is_non_positive(FUNCTION, FIELD, VALUE_0, NOT_INCLUDES_ZERO, SUPPRESS);
+    ExceptionResponse *er = ExceptionHandler_is_non_positive(FUNCTION, FIELD, VALUE_0, NOT_INCLUDES_ZERO, __SUPPRESS_PRINT_ERROR__);
     int message_comparison = strcmp(NEGATIVE_THROWS_MSG, er->error_message);
     TEST_ASSERT_EQUAL(THROWS, er->throws);
     TEST_ASSERT_EQUAL(0, message_comparison);
@@ -83,7 +82,7 @@ void test_ExceptionHandler_is_non_positive_6() {
 
 void test_ExceptionHandler_is_null_1() {
     TEST_MESSAGE("Case 1 --> value = NULL:");
-    ExceptionResponse *er = ExceptionHandler_is_null(FUNCTION, FIELD, NULL, SUPPRESS);
+    ExceptionResponse *er = ExceptionHandler_is_null(FUNCTION, FIELD, NULL, __SUPPRESS_PRINT_ERROR__);
     int message_comparison = strcmp(NULL_THROWS_MSG, er->error_message);
     TEST_ASSERT_EQUAL(THROWS, er->throws);
     TEST_ASSERT_EQUAL(0, message_comparison);
@@ -99,7 +98,7 @@ void test_ExceptionHandler_is_null_2() {
 
 void test_ExceptionHandler_is_empty_1() {
     TEST_MESSAGE("Case 1 --> EMPTY:");
-    ExceptionResponse *er = ExceptionHandler_is_empty(FUNCTION, FIELD, NULL, MOCK_is_empty_or_is_full_or_is_sorted, SUPPRESS);
+    ExceptionResponse *er = ExceptionHandler_is_empty(FUNCTION, FIELD, NULL, MOCK_is_empty_or_is_full_or_is_sorted, __SUPPRESS_PRINT_ERROR__);
     int message_comparison = strcmp(EMPTY_THROWS_MSG, er->error_message);
     TEST_ASSERT_EQUAL(THROWS, er->throws);
     TEST_ASSERT_EQUAL(0, message_comparison);
@@ -115,7 +114,7 @@ void test_ExceptionHandler_is_empty_2() {
 
 void test_ExceptionHandler_is_full_1() {
     TEST_MESSAGE("Case 1 --> FULL:");
-    ExceptionResponse *er = ExceptionHandler_is_full(FUNCTION, FIELD, NULL, MOCK_is_empty_or_is_full_or_is_sorted, SUPPRESS);
+    ExceptionResponse *er = ExceptionHandler_is_full(FUNCTION, FIELD, NULL, MOCK_is_empty_or_is_full_or_is_sorted, __SUPPRESS_PRINT_ERROR__);
     int message_comparison = strcmp(FULL_THROWS_MSG, er->error_message);
     TEST_ASSERT_EQUAL(THROWS, er->throws);
     TEST_ASSERT_EQUAL(0, message_comparison);
@@ -131,7 +130,7 @@ void test_ExceptionHandler_is_full_2() {
 
 void test_ExceptionHandler_is_not_sorted_1() {
     TEST_MESSAGE("Case 1 --> FULL:");
-    ExceptionResponse *er = ExceptionHandler_is_not_sorted(FUNCTION, FIELD, NOT_NULL, MOCK_is_empty_or_is_full_or_is_sorted, SUPPRESS);
+    ExceptionResponse *er = ExceptionHandler_is_not_sorted(FUNCTION, FIELD, NOT_NULL, MOCK_is_empty_or_is_full_or_is_sorted, __SUPPRESS_PRINT_ERROR__);
     int message_comparison = strcmp(NOT_SORTED_THROWS_MSG, er->error_message);
     TEST_ASSERT_EQUAL(THROWS, er->throws);
     TEST_ASSERT_EQUAL(0, message_comparison);
@@ -147,7 +146,7 @@ void test_ExceptionHandler_is_not_sorted_2() {
 
 void test_ExceptionHandler_is_out_of_bounds_1() {
     TEST_MESSAGE("Case 1 --> Negative index:");
-    ExceptionResponse *er = ExceptionHandler_is_out_of_bounds(FUNCTION, FIELD, VALUE_NEGATIVE_1, MAX_INDEX, SUPPRESS);
+    ExceptionResponse *er = ExceptionHandler_is_out_of_bounds(FUNCTION, FIELD, VALUE_NEGATIVE_1, MAX_INDEX, __SUPPRESS_PRINT_ERROR__);
     int message_comparison = strcmp(OUT_OF_BOUNDS_THROWS_MSG, er->error_message);
     TEST_ASSERT_EQUAL(THROWS, er->throws);
     TEST_ASSERT_EQUAL(0, message_comparison);
@@ -163,7 +162,7 @@ void test_ExceptionHandler_is_out_of_bounds_2() {
 
 void test_ExceptionHandler_is_out_of_bounds_3() {
     TEST_MESSAGE("Case 3 --> Index higher than max index allowed:");
-    ExceptionResponse *er = ExceptionHandler_is_out_of_bounds(FUNCTION, FIELD, INDEX_FIVE, MAX_INDEX, SUPPRESS);
+    ExceptionResponse *er = ExceptionHandler_is_out_of_bounds(FUNCTION, FIELD, INDEX_FIVE, MAX_INDEX, __SUPPRESS_PRINT_ERROR__);
     int message_comparison = strcmp(OUT_OF_BOUNDS_THROWS_MSG, er->error_message);
     TEST_ASSERT_EQUAL(THROWS, er->throws);
     TEST_ASSERT_EQUAL(0, message_comparison);
@@ -177,7 +176,7 @@ void test_anyThrows_1_ExceptionHandler_1() {
 
 void test_anyThrows_1_ExceptionHandler_2() {
     TEST_MESSAGE("Case 2 --> argc = 1, { ExceptionHandler->throws = true }:");
-    bool any_throws = anyThrows(1, ExceptionHandler_is_empty(FUNCTION, FIELD, NULL, MOCK_is_empty_or_is_full_or_is_sorted, SUPPRESS));
+    bool any_throws = anyThrows(1, ExceptionHandler_is_empty(FUNCTION, FIELD, NULL, MOCK_is_empty_or_is_full_or_is_sorted, __SUPPRESS_PRINT_ERROR__));
     TEST_ASSERT_EQUAL(THROWS, any_throws);
 }
 
@@ -191,7 +190,7 @@ void test_anyThrows_2_ExceptionHandlers_1() {
 
 void test_anyThrows_2_ExceptionHandlers_2() {
     ExceptionResponse *er_not_throws = ExceptionHandler_is_empty(FUNCTION, FIELD, NOT_NULL, MOCK_is_empty_or_is_full_or_is_sorted);
-    ExceptionResponse *er_throws = ExceptionHandler_is_empty(FUNCTION, FIELD, NULL, MOCK_is_empty_or_is_full_or_is_sorted, SUPPRESS);
+    ExceptionResponse *er_throws = ExceptionHandler_is_empty(FUNCTION, FIELD, NULL, MOCK_is_empty_or_is_full_or_is_sorted, __SUPPRESS_PRINT_ERROR__);
 
     TEST_MESSAGE("Case 2 --> argc = 2, { ExceptionHandler->throws = true, ExceptionHandler->throws = false }:");
     bool any_throws = anyThrows(2, er_throws, er_not_throws);
@@ -200,7 +199,7 @@ void test_anyThrows_2_ExceptionHandlers_2() {
 
 void test_anyThrows_2_ExceptionHandlers_3() {
     ExceptionResponse *er_not_throws = ExceptionHandler_is_empty(FUNCTION, FIELD, NOT_NULL, MOCK_is_empty_or_is_full_or_is_sorted);
-    ExceptionResponse *er_throws = ExceptionHandler_is_empty(FUNCTION, FIELD, NULL, MOCK_is_empty_or_is_full_or_is_sorted, SUPPRESS);
+    ExceptionResponse *er_throws = ExceptionHandler_is_empty(FUNCTION, FIELD, NULL, MOCK_is_empty_or_is_full_or_is_sorted, __SUPPRESS_PRINT_ERROR__);
 
     TEST_MESSAGE("Case 3 --> argc = 2, { ExceptionHandler->throws = false, ExceptionHandler->throws = true }:");
     bool any_throws = anyThrows(2, er_not_throws, er_throws);
@@ -208,7 +207,7 @@ void test_anyThrows_2_ExceptionHandlers_3() {
 }
 
 void test_anyThrows_2_ExceptionHandlers_4() {
-    ExceptionResponse *er_throws = ExceptionHandler_is_empty(FUNCTION, FIELD, NULL, MOCK_is_empty_or_is_full_or_is_sorted, SUPPRESS);
+    ExceptionResponse *er_throws = ExceptionHandler_is_empty(FUNCTION, FIELD, NULL, MOCK_is_empty_or_is_full_or_is_sorted, __SUPPRESS_PRINT_ERROR__);
 
     TEST_MESSAGE("Case 4 --> argc = 2, { ExceptionHandler->throws = true, ExceptionHandler->throws = true }:");
     bool any_throws = anyThrows(2, er_throws, er_throws);
@@ -225,7 +224,7 @@ void test_anyThrows_3_ExceptionHandlers_1() {
 
 void test_anyThrows_3_ExceptionHandlers_2() {
     ExceptionResponse *er_not_throws = ExceptionHandler_is_empty(FUNCTION, FIELD, NOT_NULL, MOCK_is_empty_or_is_full_or_is_sorted);
-    ExceptionResponse *er_throws = ExceptionHandler_is_empty(FUNCTION, FIELD, NULL, MOCK_is_empty_or_is_full_or_is_sorted, SUPPRESS);
+    ExceptionResponse *er_throws = ExceptionHandler_is_empty(FUNCTION, FIELD, NULL, MOCK_is_empty_or_is_full_or_is_sorted, __SUPPRESS_PRINT_ERROR__);
 
     TEST_MESSAGE("Case 2 --> argc = 3, { ExceptionHandler->throws = true, ExceptionHandler->throws = false, ExceptionHandler->throws = false }:");
     bool any_throws = anyThrows(3, er_throws, er_not_throws, er_not_throws);
@@ -234,7 +233,7 @@ void test_anyThrows_3_ExceptionHandlers_2() {
 
 void test_anyThrows_3_ExceptionHandlers_3() {
     ExceptionResponse *er_not_throws = ExceptionHandler_is_empty(FUNCTION, FIELD, NOT_NULL, MOCK_is_empty_or_is_full_or_is_sorted);
-    ExceptionResponse *er_throws = ExceptionHandler_is_empty(FUNCTION, FIELD, NULL, MOCK_is_empty_or_is_full_or_is_sorted, SUPPRESS);
+    ExceptionResponse *er_throws = ExceptionHandler_is_empty(FUNCTION, FIELD, NULL, MOCK_is_empty_or_is_full_or_is_sorted, __SUPPRESS_PRINT_ERROR__);
 
     TEST_MESSAGE("Case 3 --> argc = 3, { ExceptionHandler->throws = false, ExceptionHandler->throws = true, ExceptionHandler->throws = false }:");
     bool any_throws = anyThrows(3, er_not_throws, er_throws, er_not_throws);
@@ -243,7 +242,7 @@ void test_anyThrows_3_ExceptionHandlers_3() {
 
 void test_anyThrows_3_ExceptionHandlers_4() {
     ExceptionResponse *er_not_throws = ExceptionHandler_is_empty(FUNCTION, FIELD, NOT_NULL, MOCK_is_empty_or_is_full_or_is_sorted);
-    ExceptionResponse *er_throws = ExceptionHandler_is_empty(FUNCTION, FIELD, NULL, MOCK_is_empty_or_is_full_or_is_sorted, SUPPRESS);
+    ExceptionResponse *er_throws = ExceptionHandler_is_empty(FUNCTION, FIELD, NULL, MOCK_is_empty_or_is_full_or_is_sorted, __SUPPRESS_PRINT_ERROR__);
 
     TEST_MESSAGE("Case 4 --> argc = 3, { ExceptionHandler->throws = false, ExceptionHandler->throws = false, ExceptionHandler->throws = true }:");
     bool any_throws = anyThrows(3, er_not_throws, er_throws, er_throws);
@@ -251,7 +250,7 @@ void test_anyThrows_3_ExceptionHandlers_4() {
 }
 
 void test_anyThrows_3_ExceptionHandlers_5() {
-    ExceptionResponse *er_throws = ExceptionHandler_is_empty(FUNCTION, FIELD, NULL, MOCK_is_empty_or_is_full_or_is_sorted, SUPPRESS);
+    ExceptionResponse *er_throws = ExceptionHandler_is_empty(FUNCTION, FIELD, NULL, MOCK_is_empty_or_is_full_or_is_sorted, __SUPPRESS_PRINT_ERROR__);
 
     TEST_MESSAGE("Case 5 --> argc = 3, { ExceptionHandler->throws = true, ExceptionHandler->throws = true, ExceptionHandler->throws = true }:");
     bool any_throws = anyThrows(3, er_throws, er_throws, er_throws);
